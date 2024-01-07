64d7d8e0fdce76fb1997edf123c64f7aca91f620
Amber Parker <amberparker@arizona.edu>
2024-01-07 15:47:23 -0700

    1

--- Exports/Recorded Data For Paper/new_parse.m
+++ Exports/Recorded Data For Paper/new_parse.m
@@ -1,14 +1,15 @@
- 
 clear; clc; close all;
-data = readmatrix("coordinates1.csv");
+data = readmatrix("coordinates_1_7_306.csv");
 %changed cvsread because of warning to readmatrix
 figure;
+
 plot3(data(:, 1), data(:, 2), -1.0 * data(:, 3));
+plot3(data(:,4),data(:, 5), -1.0 * data(:, 6));
 title('Drone Positions','Color',[0 0 1],'FontSize', 15)
 xlabel("X-Axis","FontSize",10,"Color",[0 0 1]);
 ylabel("Y-Axis","FontSize",10,"Color",[0 0 1]);
 zlabel("Z-Axis","FontSize",10,"Color",[0 0 1]);
-xlim([-3 5]);
-ylim([-3 5]);
-zlim([-3 5]);
+xlim([-4 15]);
+ylim([-4 15]);
+zlim([-50 15]);
 grid on
